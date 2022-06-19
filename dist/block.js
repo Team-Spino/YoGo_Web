f5e48defd1f18c3fbe5a87efd1de321b8c7cf5f3
KimSeungDeok <tmdejr1117@gmail.com>
2022-06-20 01:55:11 +0900

    feat : 화면 줄였을때 block하는 기능 추가

---
+++ dist/block.js
@@ -0,0 +1,29 @@
+console.log(window.screen.width+"x"+window.screen.height); // screen dimension
+console.log(window.screen.availWidth+"x"+window.screen.availHeight); //exclude os taskbar
+console.log(window.outerWidth+"x"+window.outerHeight); // browser size
+console.log(window.innerWidth+"x"+window.innerHeight); // viewport size
+
+
+const container = document.querySelectorAll('.container');
+
+console.log(container[0].innerHTML);
+const container0 = container[0].innerHTML;
+const container1 = container[1].innerHTML;
+const container2 = container[2].innerHTML;
+const container3 = container[3].innerHTML;
+
+function checkSize(){
+    if(window.innerWidth <= 1200 && window.innerWidth >= 597){
+        for(let i = 0; i < container.length; i++){
+            container[i].innerHTML = '화면을 확대해주세요!😅';
+        }
+    }
+    else {
+        container[0].innerHTML = container0;
+        container[1].innerHTML = container1;
+        container[2].innerHTML = container2;
+        container[3].innerHTML = container3;
+    }
+}
+
+setInterval(checkSize, 100);
